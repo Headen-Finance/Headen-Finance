8252068d5eaf521d454df7ddf3d519bf755854e8
Kodam <kolozsvariadam@gmail.com>
2022-09-14 22:57:59 +0200

    feat: new layout with header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,30 +1,43 @@
+import { showNotification } from '@mantine/notifications';
 import * as React from 'react';
 
 import UnstyledLink from '@/components/links/UnstyledLink';
 
-const links = [
-  { href: '/', label: 'Route 1' },
-  { href: '/', label: 'Route 2' },
-];
-
 export default function Header() {
   return (
-    <header className='sticky top-0 z-50 bg-white'>
+    <header className='sticky top-0 z-50 bg-black text-white'>
       <div className='layout flex h-14 items-center justify-between'>
         <UnstyledLink href='/' className='font-bold hover:text-gray-600'>
-          Home
+          Headen Finance
         </UnstyledLink>
-        <nav>
-          <ul className='flex items-center justify-between space-x-4'>
-            {links.map(({ href, label }) => (
-              <li key={`${href}${label}`}>
-                <UnstyledLink href={href} className='hover:text-gray-600'>
-                  {label}
-                </UnstyledLink>
-              </li>
-            ))}
-          </ul>
-        </nav>
+        <UnstyledLink href='/' className='font-bold hover:text-gray-600'>
+          Dashboard
+        </UnstyledLink>
+        <UnstyledLink
+          onClick={() =>
+            showNotification({
+              title: 'Oops',
+              message: 'Hey there!',
+              color: 'green',
+              loading: true,
+            })
+          }
+          href='/'
+          className='font-bold hover:text-gray-600'
+        >
+          User Account
+        </UnstyledLink>
+        {/*<nav>*/}
+        {/*  <ul className='flex items-center justify-between space-x-4'>*/}
+        {/*    {links.map(({ href, label }) => (*/}
+        {/*      <li key={`${href}${label}`}>*/}
+        {/*        <UnstyledLink href={href} className='hover:text-gray-600'>*/}
+        {/*          {label}*/}
+        {/*        </UnstyledLink>*/}
+        {/*      </li>*/}
+        {/*    ))}*/}
+        {/*  </ul>*/}
+        {/*</nav>*/}
       </div>
     </header>
   );
