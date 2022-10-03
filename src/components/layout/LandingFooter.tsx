f4faa6ec575c618896e81dc9dbed014361794308
Kodam <kolozsvariadam@gmail.com>
2022-10-03 21:52:31 +0200

    feat: footer links

--- src/components/layout/LandingFooter.tsx
+++ src/components/layout/LandingFooter.tsx
@@ -1,3 +1,5 @@
+import UnstyledLink from '@/components/links/UnstyledLink';
+
 export const LandingFooter = () => {
   return (
     <footer className='flex flex-col justify-center bg-[#1B1D23]'>
@@ -9,17 +11,53 @@ export const LandingFooter = () => {
             Finance
           </p>
         </div>
-        <div>
+        <div className='flex flex-col gap-6'>
           <h5 className='font-bold'>Markets</h5>
+          {Array.of(...Array(5)).map((value) => (
+            <UnstyledLink
+              key={value}
+              href='#'
+              className='block text-sm font-semibold text-gray-500 hover:text-secondary'
+            >
+              eTokens
+            </UnstyledLink>
+          ))}
         </div>
-        <div>
+        <div className='flex flex-col gap-6'>
           <h5 className='font-bold'>Docs</h5>
+          {Array.of(...Array(5)).map((value) => (
+            <UnstyledLink
+              key={value}
+              href='#'
+              className='block text-sm font-semibold text-gray-500 hover:text-secondary'
+            >
+              eTokens
+            </UnstyledLink>
+          ))}
         </div>
-        <div>
+        <div className='flex flex-col gap-6'>
           <h5 className='font-bold'>FAQ</h5>
+          {Array.of(...Array(5)).map((value) => (
+            <UnstyledLink
+              key={value}
+              href='#'
+              className='block text-sm font-semibold text-gray-500 hover:text-secondary'
+            >
+              eTokens
+            </UnstyledLink>
+          ))}
         </div>
-        <div>
+        <div className='flex flex-col gap-6'>
           <h5 className='font-bold'>Security</h5>
+          {Array.of(...Array(5)).map((value) => (
+            <UnstyledLink
+              key={value}
+              href='#'
+              className='block text-sm font-semibold text-gray-500 hover:text-secondary'
+            >
+              eTokens
+            </UnstyledLink>
+          ))}
         </div>
       </div>
       <div className=' mx-10 mb-20 border-b border-[#E2E2E233]'></div>
