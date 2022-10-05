c24f202742ee24b4aace34596143c77bc9a833e6
Kodam <kolozsvariadam@gmail.com>
2022-10-05 22:21:19 +0200

    perf: remove colors.css

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,6 @@
 import { AppProps } from 'next/app';
 
 import '@/styles/globals.css';
-// !STARTERCONF This is for demo purposes, remove @/styles/colors.css import immediately
-import '@/styles/colors.css';
 
 import { CustomToaster } from '@/components/toaster/CustomToaster';
 import Web3Provider from '@/components/web3/Web3Provider';
