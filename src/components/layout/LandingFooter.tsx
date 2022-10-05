91dd7f6cbd01fed5c4e6771e38db74635a9c58fb
Kodam <kolozsvariadam@gmail.com>
2022-10-05 19:52:06 +0200

    chore: footer refactor

--- src/components/layout/LandingFooter.tsx
+++ src/components/layout/LandingFooter.tsx
@@ -1,66 +1,84 @@
-import UnstyledLink from '@/components/links/UnstyledLink';
+import * as React from 'react';
+
+import clsxm from '@/lib/clsxm';
+
+import UnstyledLink, {
+  UnstyledLinkProps,
+} from '@/components/links/UnstyledLink';
 
 export const LandingFooter = () => {
   return (
     <footer className='flex flex-col justify-center bg-[#1B1D23]'>
       <div className='lg:p-30 grid w-full max-w-screen-2xl grid-cols-1 gap-4 p-16 text-white sm:grid-cols-2 md:grid-cols-3 md:p-20 lg:grid-cols-5 xl:p-40 '>
-        <div>
+        <FooterSection>
           <p className='mx-auto text-center text-xl font-bold'>
             Headen.
             <br />
             Finance
           </p>
-        </div>
-        <div className='flex flex-col gap-6 py-4'>
-          <h5 className='text-center font-bold'>Markets</h5>
+        </FooterSection>
+        <FooterSection title='Markets'>
           {Array.of(...Array(5)).map((value) => (
-            <UnstyledLink
-              key={value}
-              href='#'
-              className='mx-auto block text-sm font-semibold text-gray-500 hover:text-secondary'
-            >
+            <LandingLink key={value} href='#'>
               eTokens
-            </UnstyledLink>
+            </LandingLink>
           ))}
-        </div>
-        <div className='flex flex-col gap-6 py-4'>
-          <h5 className='text-center font-bold'>Docs</h5>
+        </FooterSection>
+        <FooterSection title='Docs'>
           {Array.of(...Array(5)).map((value) => (
-            <UnstyledLink
-              key={value}
-              href='#'
-              className='mx-auto block text-sm font-semibold text-gray-500 hover:text-secondary'
-            >
+            <LandingLink key={value} href='#'>
               eTokens
-            </UnstyledLink>
+            </LandingLink>
           ))}
-        </div>
-        <div className='flex flex-col gap-6 py-4'>
-          <h5 className='text-center font-bold'>FAQ</h5>
+        </FooterSection>
+        <FooterSection title='FAQ'>
           {Array.of(...Array(5)).map((value) => (
-            <UnstyledLink
-              key={value}
-              href='#'
-              className='mx-auto block text-sm font-semibold text-gray-500 hover:text-secondary'
-            >
+            <LandingLink key={value} href='#'>
               eTokens
-            </UnstyledLink>
+            </LandingLink>
           ))}
-        </div>
-        <div className='flex flex-col gap-6 py-4'>
-          <h5 className='text-center font-bold'>Security</h5>
+        </FooterSection>
+        <FooterSection title='Security'>
           {Array.of(...Array(5)).map((value) => (
-            <UnstyledLink
-              key={value}
-              href='#'
-              className='mx-auto block text-sm font-semibold text-gray-500 hover:text-secondary'
-            >
+            <LandingLink key={value} href='#'>
               eTokens
-            </UnstyledLink>
+            </LandingLink>
           ))}
-        </div>
+        </FooterSection>
       </div>
       <div className=' mx-10 mb-20 border-b border-[#E2E2E233]'></div>
     </footer>
   );
 };
+
+type FooterSectionProps = {
+  title?: string;
+} & React.ComponentPropsWithoutRef<'div'>;
+
+function FooterSection({
+  title,
+  children,
+  className,
+  ...rest
+}: FooterSectionProps) {
+  return (
+    <div className={clsxm('flex flex-col gap-6 py-4', className)} {...rest}>
+      {title && <h5 className='text-center font-bold'>{title}</h5>}
+      {children}
+    </div>
+  );
+}
+
+function LandingLink({ children, className, ...rest }: UnstyledLinkProps) {
+  return (
+    <UnstyledLink
+      className={clsxm(
+        'mx-auto block text-sm font-semibold text-gray-500 hover:text-secondary',
+        className
+      )}
+      {...rest}
+    >
+      {children}
+    </UnstyledLink>
+  );
+}
