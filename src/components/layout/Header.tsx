48f5731d5e1a69e240afa07cc0762421cca0706d
Kodam <kolozsvariadam@gmail.com>
2022-10-06 22:11:43 +0200

    feat: hamburger menu to headers

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,13 +1,33 @@
 import * as React from 'react';
 
+import HamburgerMenu from '@/components/layout/HamburgerMenu';
 import UnstyledLink from '@/components/links/UnstyledLink';
 import { ConnectWallet } from '@/components/web3/ConnectWallet';
+const menuItems = [
+  {
+    href: '#',
+    title: 'Stake',
+  },
+  {
+    href: '#',
+    title: 'Borrow',
+  },
+  {
+    href: '#',
+    title: 'Withdraw',
+  },
+  {
+    href: '#',
+    title: 'Repay',
+  },
+];
 
 export default function Header() {
   return (
     <header className='sticky top-0 z-50 bg-[#0E1118] text-white'>
       <div className='layout flex h-14 items-center justify-between'>
-        <div className='grow basis-0'>
+        <div className='flex grow basis-0 flex-nowrap items-center'>
+          <HamburgerMenu menuItems={menuItems} />
           <UnstyledLink href='/' className='font-bold hover:text-gray-600'>
             Headen Finance
           </UnstyledLink>
--- src/components/layout/LandingHeader.tsx
+++ src/components/layout/LandingHeader.tsx
@@ -1,10 +1,9 @@
-import { Popover, Transition } from '@headlessui/react';
 import * as React from 'react';
-import { Fragment, useEffect, useState } from 'react';
-import { MdClose, MdMenu } from 'react-icons/md';
+import { useEffect, useState } from 'react';
 
 import clsxm from '@/lib/clsxm';
 
+import HamburgerMenu from '@/components/layout/HamburgerMenu';
 import UnstyledLink from '@/components/links/UnstyledLink';
 
 const menuItems = [
@@ -50,51 +49,7 @@ export default function LandingHeader() {
     >
       <div className='mx-auto grid h-20 max-w-screen-2xl grid-cols-2 items-center justify-between px-5 transition-[background] sm:px-10 md:grid-cols-[200px_1fr_200px]'>
         <div className='flex items-center'>
-          <Popover className='relative block md:hidden'>
-            {({ open }) => (
-              <>
-                <Popover.Button
-                  className={`
-                ${open ? '' : 'text-opacity-90'}
-                group mr-1 inline-flex items-center rounded-md  text-base text-white hover:text-opacity-100 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75`}
-                >
-                  {open ? (
-                    <MdClose className='m-2 inline-block flex-shrink-0 cursor-pointer text-lg'></MdClose>
-                  ) : (
-                    <MdMenu className='m-2 inline-block flex-shrink-0 cursor-pointer text-lg'></MdMenu>
-                  )}
-                </Popover.Button>
-                <Transition
-                  as={Fragment}
-                  enter='transition ease-out duration-200'
-                  enterFrom='opacity-0 translate-y-1'
-                  enterTo='opacity-100 translate-y-0'
-                  leave='transition ease-in duration-150'
-                  leaveFrom='opacity-100 translate-y-0'
-                  leaveTo='opacity-0 translate-y-1'
-                >
-                  <Popover.Panel className='fixed left-0 right-0 z-10 mt-3 max-w-xl transform px-5 sm:px-10'>
-                    <div>
-                      <div className='overflow-hidden rounded-lg shadow-lg ring-1 ring-black ring-opacity-5'>
-                        <nav className='bg-gray-50 p-4'>
-                          {menuItems.map((value) => (
-                            <Popover.Button
-                              key={value.title}
-                              href={value.href}
-                              className='block rounded-md p-4 text-sm font-semibold text-primary hover:bg-primary hover:text-secondary'
-                              as={UnstyledLink}
-                            >
-                              {value.title}
-                            </Popover.Button>
-                          ))}
-                        </nav>
-                      </div>
-                    </div>
-                  </Popover.Panel>
-                </Transition>
-              </>
-            )}
-          </Popover>
+          <HamburgerMenu menuItems={menuItems} />
           <UnstyledLink
             href='/'
             className='text-lg font-semibold  hover:text-secondary sm:text-xl'
