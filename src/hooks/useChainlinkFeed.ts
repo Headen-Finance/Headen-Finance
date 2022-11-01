b9fab572d6d16b3d64249d87380849ff40648469
Kodam <kolozsvariadam@gmail.com>
2022-11-01 19:00:52 +0100

    feat: chainlink feed

--- src/hooks/useChainlinkFeed.ts
+++ src/hooks/useChainlinkFeed.ts
@@ -1,83 +1,99 @@
-import { useCallback, useEffect, useState } from 'react';
-import { useContract, useProvider } from 'wagmi';
-const addr = '0x48731cF7e84dc94C5f84577882c14Be11a5B7456';
+import { BigNumber } from "ethers";
+import { useCallback, useEffect, useState } from "react";
+import { useContract, useContractRead, useProvider } from "wagmi";
+// const addr = '0x48731cF7e84dc94C5f84577882c14Be11a5B7456';
+const addr = "0xfe4a8cc5b5b2366c1b58bea3858e81843581b2f7"; //polygon
 const aggregatorV3InterfaceABI = [
   {
     inputs: [],
-    name: 'decimals',
-    outputs: [{ internalType: 'uint8', name: '', type: 'uint8' }],
-    stateMutability: 'view',
-    type: 'function',
+    name: "decimals",
+    outputs: [{ internalType: "uint8", name: "", type: "uint8" }],
+    stateMutability: "view",
+    type: "function",
   },
   {
     inputs: [],
-    name: 'description',
-    outputs: [{ internalType: 'string', name: '', type: 'string' }],
-    stateMutability: 'view',
-    type: 'function',
+    name: "description",
+    outputs: [{ internalType: "string", name: "", type: "string" }],
+    stateMutability: "view",
+    type: "function",
   },
   {
-    inputs: [{ internalType: 'uint80', name: '_roundId', type: 'uint80' }],
-    name: 'getRoundData',
+    inputs: [{ internalType: "uint80", name: "_roundId", type: "uint80" }],
+    name: "getRoundData",
     outputs: [
-      { internalType: 'uint80', name: 'roundId', type: 'uint80' },
-      { internalType: 'int256', name: 'answer', type: 'int256' },
-      { internalType: 'uint256', name: 'startedAt', type: 'uint256' },
-      { internalType: 'uint256', name: 'updatedAt', type: 'uint256' },
-      { internalType: 'uint80', name: 'answeredInRound', type: 'uint80' },
+      { internalType: "uint80", name: "roundId", type: "uint80" },
+      { internalType: "int256", name: "answer", type: "int256" },
+      { internalType: "uint256", name: "startedAt", type: "uint256" },
+      { internalType: "uint256", name: "updatedAt", type: "uint256" },
+      { internalType: "uint80", name: "answeredInRound", type: "uint80" },
     ],
-    stateMutability: 'view',
-    type: 'function',
+    stateMutability: "view",
+    type: "function",
   },
   {
     inputs: [],
-    name: 'latestRoundData',
+    name: "latestRoundData",
     outputs: [
-      { internalType: 'uint80', name: 'roundId', type: 'uint80' },
-      { internalType: 'int256', name: 'answer', type: 'int256' },
-      { internalType: 'uint256', name: 'startedAt', type: 'uint256' },
-      { internalType: 'uint256', name: 'updatedAt', type: 'uint256' },
-      { internalType: 'uint80', name: 'answeredInRound', type: 'uint80' },
+      { internalType: "uint80", name: "roundId", type: "uint80" },
+      { internalType: "int256", name: "answer", type: "int256" },
+      { internalType: "uint256", name: "startedAt", type: "uint256" },
+      { internalType: "uint256", name: "updatedAt", type: "uint256" },
+      { internalType: "uint80", name: "answeredInRound", type: "uint80" },
     ],
-    stateMutability: 'view',
-    type: 'function',
+    stateMutability: "view",
+    type: "function",
   },
   {
     inputs: [],
-    name: 'version',
-    outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
-    stateMutability: 'view',
-    type: 'function',
+    name: "version",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
   },
-];
+] as const;
 
 export enum FeedState {
-  INIT = 'INIT',
-  FETCHING = 'FETCHING',
-  FETCHED = 'FETCHED',
-  ERROR = 'ERROR',
+  INIT = "INIT",
+  FETCHING = "FETCHING",
+  FETCHED = "FETCHED",
+  ERROR = "ERROR",
 }
 
-export function useChainlinkFeed() {
+export function useChainlinkFeedData() {
+  const latestRoundData = useContractRead({
+    address: addr,
+    abi: aggregatorV3InterfaceABI,
+    functionName: "latestRoundData",
+    cacheTime: 10000,
+  });
+  return latestRoundData?.data?.answer;
+}
+
+export const useChainlinkContract = () => {
   const provider = useProvider();
-  const [lastRoundData, setLastRoundData] = useState<Record<string, unknown>>();
-  const [state, setState] = useState(FeedState.INIT);
-  const priceFeed = useContract({
+  return useContract({
     address: addr,
     abi: aggregatorV3InterfaceABI,
     signerOrProvider: provider,
   });
+};
+
+export function useChainlinkFeed() {
+  const [lastRoundData, setLastRoundData] = useState<BigNumber>();
+  const [state, setState] = useState(FeedState.INIT);
+  const priceFeed = useChainlinkContract();
 
   const refresh = useCallback(() => {
     setState(FeedState.FETCHING);
     priceFeed
       ?.latestRoundData()
-      ?.then((roundData: Record<string, unknown>) => {
+      ?.then((roundData) => {
         setState(FeedState.FETCHED);
-        setLastRoundData(roundData);
+        setLastRoundData(roundData.answer);
         // Do something with roundData
         // eslint-disable-next-line no-console
-        console.log('Latest Round Data', roundData);
+        console.log("Latest Round Data", roundData);
       })
       ?.catch(() => {
         setState(FeedState.ERROR);
