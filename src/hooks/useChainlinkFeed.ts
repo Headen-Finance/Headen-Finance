345dbf558a29383f3f61b42d88bcf9f38e4b9ef6
Kodam <kolozsvariadam@gmail.com>
2022-11-05 09:31:35 +0100

    feat: multichain config for chainlink feed

--- src/hooks/useChainlinkFeed.ts
+++ src/hooks/useChainlinkFeed.ts
@@ -1,8 +1,6 @@
-import { BigNumber } from "ethers";
-import { useCallback, useEffect, useState } from "react";
-import { useContract, useContractRead, useProvider } from "wagmi";
-// const addr = '0x48731cF7e84dc94C5f84577882c14Be11a5B7456';
-const addr = "0xfe4a8cc5b5b2366c1b58bea3858e81843581b2f7"; //polygon
+import { useContractRead } from "wagmi";
+
+import { useChainData } from "@/hooks/useChainData";
 const aggregatorV3InterfaceABI = [
   {
     inputs: [],
@@ -53,55 +51,57 @@ const aggregatorV3InterfaceABI = [
   },
 ] as const;
 
-export enum FeedState {
-  INIT = "INIT",
-  FETCHING = "FETCHING",
-  FETCHED = "FETCHED",
-  ERROR = "ERROR",
-}
+// export enum FeedState {
+//   INIT = "INIT",
+//   FETCHING = "FETCHING",
+//   FETCHED = "FETCHED",
+//   ERROR = "ERROR",
+// }
 
-export function useChainlinkFeedData() {
+export function useChainlinkFeedData({ enabled }: { enabled?: boolean }) {
+  const { chainlinkUsdcFeed } = useChainData();
   const latestRoundData = useContractRead({
-    address: addr,
+    address: chainlinkUsdcFeed,
     abi: aggregatorV3InterfaceABI,
     functionName: "latestRoundData",
     staleTime: 10000,
+    enabled: enabled,
   });
   return latestRoundData?.data?.answer;
 }
-
-export const useChainlinkContract = () => {
-  const provider = useProvider();
-  return useContract({
-    address: addr,
-    abi: aggregatorV3InterfaceABI,
-    signerOrProvider: provider,
-  });
-};
-
-export function useChainlinkFeed() {
-  const [lastRoundData, setLastRoundData] = useState<BigNumber>();
-  const [state, setState] = useState(FeedState.INIT);
-  const priceFeed = useChainlinkContract();
-
-  const refresh = useCallback(() => {
-    setState(FeedState.FETCHING);
-    priceFeed
-      ?.latestRoundData()
-      ?.then((roundData) => {
-        setState(FeedState.FETCHED);
-        setLastRoundData(roundData.answer);
-        // Do something with roundData
-        // eslint-disable-next-line no-console
-        console.log("Latest Round Data", roundData);
-      })
-      ?.catch(() => {
-        setState(FeedState.ERROR);
-        setLastRoundData(undefined);
-      });
-  }, [priceFeed]);
-
-  useEffect(() => refresh(), [refresh]);
-
-  return { lastRoundData, state, refresh };
-}
+//
+// export const useChainlinkContract = () => {
+//   const provider = useProvider();
+//   return useContract({
+//     address: addr,
+//     abi: aggregatorV3InterfaceABI,
+//     signerOrProvider: provider,
+//   });
+// };
+//
+// export function useChainlinkFeed() {
+//   const [lastRoundData, setLastRoundData] = useState<BigNumber>();
+//   const [state, setState] = useState(FeedState.INIT);
+//   const priceFeed = useChainlinkContract();
+//
+//   const refresh = useCallback(() => {
+//     setState(FeedState.FETCHING);
+//     priceFeed
+//       ?.latestRoundData()
+//       ?.then((roundData) => {
+//         setState(FeedState.FETCHED);
+//         setLastRoundData(roundData.answer);
+//         // Do something with roundData
+//         // eslint-disable-next-line no-console
+//         console.log("Latest Round Data", roundData);
+//       })
+//       ?.catch(() => {
+//         setState(FeedState.ERROR);
+//         setLastRoundData(undefined);
+//       });
+//   }, [priceFeed]);
+//
+//   useEffect(() => refresh(), [refresh]);
+//
+//   return { lastRoundData, state, refresh };
+// }
